16ab1ed49557b1ee6833df9c405cdd5f750f33c0
Kyle Smith <ksmith@wspackaging.com>
2014-04-20 09:09:23 -0500

    require jar files with ease

--- config/environment.rb
+++ config/environment.rb
@@ -3,34 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 require 'sequel'
 
-# open edge driver 10.2B
+# require all jar files in lib
+Dir["../../lib/*.jar"].each {|file| require file }
+
+# open edge driver 10.2B / of course Progress/OpenEdge requires special treatment
 if File.exists?(File.expand_path('../../lib/openedge.jar', __FILE__)) &&
    File.exists?(File.expand_path('../../lib/pool.jar', __FILE__))
 
-  require 'lib/openedge.jar'
-  require 'lib/pool.jar'
   java_import 'com.ddtek.jdbc.openedge.OpenEdgeDriver'
 
-end
-
-# jtds driver
-if File.exists?(File.expand_path('../../lib/jtds.jar', __FILE__))
-  require 'lib/jtds.jar'
-end
-
-# mysql driver
-if File.exists?(File.expand_path('../../lib/mysql.jar', __FILE__))
-  require 'lib/mysql.jar'
-end
-
-# postgresql driver
-if File.exists?(File.expand_path('../../lib/postgresql.jar', __FILE__))
-  require 'lib/postgresql.jar'
-end
-
-# sqlite3 driver
-if File.exists?(File.expand_path('../../lib/sqlite.jar', __FILE__))
-  require 'lib/sqlite.jar'
 end
 
 ENV['FREETDSCONF'] = File.expand_path(File.dirname(__FILE__)) + "/freetds.conf"
