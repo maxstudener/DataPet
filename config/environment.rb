e376b49ac77c2fcb6a20affc88d16aa1c6d9cad2
Kyle Smith <ksmith@wspackaging.com>
2014-04-20 09:15:59 -0500

    when things work locally, but not on server :)

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,12 @@ require File.expand_path('../application', __FILE__)
 require 'sequel'
 
 # require all jar files in lib
-Dir["../../lib/*.jar"].each {|file| require file }
+Dir["../../lib/*.jar"].each {|file| require "lib/#{file}" }
 
 # open edge driver 10.2B / Progress needs special treatment
 if File.exists?(File.expand_path('../../lib/openedge.jar', __FILE__)) &&
    File.exists?(File.expand_path('../../lib/pool.jar', __FILE__))
 
-  require 'lib/openedge.jar'
-  require 'lib/pool.jar'
   java_import 'com.ddtek.jdbc.openedge.OpenEdgeDriver'
 
 end
